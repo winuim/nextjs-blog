b0de067d0da6667187c8a3e2abfd033dc970818d
Uema Yohei <winuim@gmail.com>
2021-02-14 22:35:30 +0900

    rename api

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export const handler = (_: NextApiRequest, res: NextApiResponse): void => {
+export const hello = (_: NextApiRequest, res: NextApiResponse): void => {
   res.status(200).json({ text: "Hello" });
 };
 
-export default handler;
+export default hello;
--- test/pages/api/hello.test.ts
+++ test/pages/api/hello.test.ts
@@ -1,12 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import httpMocks from "node-mocks-http";
-import { handler } from "../../../pages/api/hello";
+import { hello } from "../../../pages/api/hello";
 
 describe("/api/home handler", () => {
   test("responds 200 GET", async () => {
     const mockReq = httpMocks.createRequest<NextApiRequest>();
     const mockRes = httpMocks.createResponse<NextApiResponse>();
-    await handler(mockReq, mockRes);
+    await hello(mockReq, mockRes);
     expect(mockRes.statusCode).toEqual(200);
     expect(mockRes._getJSONData()).toEqual({ text: "Hello" });
   });
