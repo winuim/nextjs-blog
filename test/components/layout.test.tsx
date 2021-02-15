31eede1c6dc5753ad7e6400053c22decb7688d49
Uema Yohei <winuim@gmail.com>
2021-02-15 21:13:53 +0900

    add layout, posts test code

---
+++ test/components/layout.test.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { render } from "../testUtils";
+import Layout from "../../components/layout";
+
+describe("Layout component", () => {
+  it("matches snapshot", () => {
+    const { asFragment } = render(
+      <Layout>
+        <span>test</span>
+      </Layout>,
+      {}
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+  it("matches snapshot, home", () => {
+    const { asFragment } = render(
+      <Layout home>
+        <span>test</span>
+      </Layout>,
+      {}
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
---
+++ test/lib/posts.test.ts
@@ -0,0 +1,30 @@
+import {
+  getSortedPostsData,
+  getAllPostIds,
+  getPostData,
+} from "../../lib/posts";
+
+describe("Posts component", () => {
+  it("getSortedPostsData", () => {
+    const sortedPostIds = getSortedPostsData();
+    sortedPostIds.forEach((v) => {
+      expect(v).toHaveProperty("date");
+      expect(v).toHaveProperty("title");
+      expect(v).toHaveProperty("id");
+    });
+  });
+  it("getAllPostIds", () => {
+    const allPostIds = getAllPostIds();
+    allPostIds.forEach((v) => {
+      expect(v).toHaveProperty("params");
+      expect(v.params).toHaveProperty("id");
+    });
+  });
+  it("getPostData", async () => {
+    const postData = await getPostData("pre-rendering");
+    expect(postData).toHaveProperty("date");
+    expect(postData).toHaveProperty("title");
+    expect(postData).toHaveProperty("id");
+    expect(postData).toHaveProperty("contentHtml");
+  });
+});
