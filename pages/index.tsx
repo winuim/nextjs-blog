ea24d770ef31531c2a828b07176907a3b39f6f39
Uema Yohei <winuim@gmail.com>
2021-02-26 22:30:01 +0900

    rename

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,64 +1,27 @@
-import Head from 'next/head';
-import utilStyles from '../styles/utils.module.css';
-import {getSortedPostsData} from '../lib/posts';
-import Link from 'next/link';
-import Date from '../components/date';
-import Layout, {siteTitle} from '../components/layout';
-import {GetStaticProps} from 'next';
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import ProTip from '../components/protip';
+import Link from '../components/link';
+import Copyright from '../components/copyright';
 
-export default function Home({
-  allPostsData,
-}: {
-  allPostsData: {
-    date: string;
-    title: string;
-    id: string;
-  }[];
-}): JSX.Element {
+export default function Index() {
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-      <button
-        onClick={() => {
-          window.alert('With typescript and Jest');
-        }}
-      >
-        Test Button
-      </button>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({id, date, title}) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Next.js with TypeScript example
+        </Typography>
+        <Link href="/home" color="secondary">
+          Go to the blog page
+        </Link>
+        <Link href="/about" color="secondary">
+          Go to the about page
+        </Link>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
   );
 }
-
-export const getStaticProps: GetStaticProps = async () => {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-};
